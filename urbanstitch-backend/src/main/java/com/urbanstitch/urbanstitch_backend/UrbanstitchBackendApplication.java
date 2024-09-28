8786e48e331373abed0f44be4ffdf70cb9eded72
Sougat Ganguly <ganguly2804@gmail.com>
2024-09-28 18:20:05 +0530

    First commit

---
+++ urbanstitch-backend/src/main/java/com/urbanstitch/urbanstitch_backend/UrbanstitchBackendApplication.java
@@ -0,0 +1,14 @@
+package com.urbanstitch.urbanstitch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanstitchBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		SpringApplication.run(UrbanstitchBackendApplication.class, args);
+	}
+
+}
---
+++ urbanstitch-backend/src/test/java/com/urbanstitch/urbanstitch_backend/UrbanstitchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanstitch.urbanstitch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanstitchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
